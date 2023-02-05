c97cda4151523af7de5d2163f0231947e5351763
Mao De Matos <mao.dematos@gmail.com>
2023-02-05 17:11:40 +0100

    ♻️ 💄 Refactor styles with `tailwindcss`

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,30 @@
+const plugin = require("tailwindcss/plugin");
+
+/**
+ * Add `hover:` + `focus:` selectors
+ *
+ * Built using https://github.com/soorria/tailwindcss-hocus
+ */
+const hocusSelectors = plugin(function ({ addVariant }) {
+  // Normal selectors
+  const hoverSelector = "&:hover";
+  addVariant("hocus", [hoverSelector, "&:focus"]);
+  addVariant("hocus-within", [hoverSelector, "&:focus-within"]);
+  addVariant("hocus-visible", [hoverSelector, "&:focus-visible"]);
+
+  // Group selectors
+  const groupHoverSelector = ":merge(.group):hover &";
+  addVariant("group-hocus", [groupHoverSelector, ":merge(.group):focus &"]);
+  addVariant("group-hocus-within", [
+    groupHoverSelector,
+    ":merge(.group):focus-within &",
+  ]);
+  addVariant("group-hocus-visible", [
+    groupHoverSelector,
+    ":merge(.group):focus-visible &",
+  ]);
+});
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -7,9 +34,9 @@ module.exports = {
         accent: "hsl(var(--accent) / <alpha-value>)",
       },
       backgroundImage: {
-        "brand-gradient": "var(--accent-gradient)",
+        "brand-gradient": "var(--brand-gradient)",
       },
     },
   },
-  plugins: [],
+  plugins: [hocusSelectors],
 };
